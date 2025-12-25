743993330b3fb7444a2fe36972cc231a09a052ce
scttpr <git.reformer354@passmail.net>
2025-12-25 09:14:53 +0100

    feat(service): add follow service with follow/unfollow/counts

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,11 +1,17 @@
-// Services module - RecipeService, BookService, ActivityService, FederationService
+// Services module - RecipeService, BookService, ActivityService, SocialService
 
+pub mod activity_service;
 pub mod book_service;
+pub mod follow_service;
 pub mod image_service;
 pub mod recipe_service;
+pub mod saved_recipe_service;
 pub mod user_service;
 
+pub use activity_service::ActivityService;
 pub use book_service::BookService;
+pub use follow_service::FollowService;
 pub use image_service::ImageService;
 pub use recipe_service::RecipeService;
+pub use saved_recipe_service::SavedRecipeService;
 pub use user_service::UserService;
